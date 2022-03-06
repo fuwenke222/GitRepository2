f7ef6f0c23862e8e46b7e1685133aef70139ffa1
fwk <3577539873@qq.com>
2022-03-06 19:55:14 +0800

    在Person类中增加了id属性

---
+++ TestModule01/src/com/fwk/test/Person.java
@@ -0,0 +1,11 @@
+package com.fwk.test;
+
+/**
+ * @author : fuwenk
+ * @date: 2022/3/6 - 03 - 06 - 19:53
+ * @Description: com.fwk.test
+ * @version: 1.0
+ */
+public class Person {
+    private Integer id;
+}
